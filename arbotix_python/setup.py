afcc260ed3364a43e5a354ac6e2c5a4b072ab66f
eataiwo <t_taiwo@live.co.uk>
2023-10-04 14:03:46 +0100

    Update: Deleting new custom firmware for github repo clean up

--- arbotix_python/setup.py
+++ arbotix_python/setup.py
@@ -6,7 +6,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 d = generate_distutils_setup(
     scripts=['bin/arbotix_gui', 'bin/arbotix_terminal'],
     packages=['arbotix_python'],
-    package_dir={'': 'src'},
+    package_dir={'': 'sqash'},
     )
 
 setup(**d)
--- arbotix_sensors/setup.py
+++ arbotix_sensors/setup.py
@@ -5,7 +5,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
     packages=['arbotix_sensors'],
-    package_dir={'': 'src'},
+    package_dir={'': 'sqash'},
     )
 
 setup(**d)
